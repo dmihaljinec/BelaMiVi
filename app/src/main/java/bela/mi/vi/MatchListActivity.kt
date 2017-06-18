c9ba7e88fc16e605d07a4b1150b69d7675654f85
Damir Mihaljinec <damir.mihaljinec@gmail.com>
2017-06-18 17:13:19 +0200

    Project setup - added single activity

---
+++ app/src/main/java/bela/mi/vi/MatchListActivity.kt
@@ -0,0 +1,12 @@
+package bela.mi.vi
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MatchListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
