b52cc0072f82a698b73f863b525c5e738b37a4a3
Damir Mihaljinec <damir.mihaljinec@qnective.com>
2020-05-21 13:02:33 +0200

    Updated build tools Switched from support to androidx

--- app/src/main/java/bela/mi/vi/MatchListActivity.kt
+++ app/src/main/java/bela/mi/vi/MatchListActivity.kt
@@ -1,6 +1,6 @@
 package bela.mi.vi
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MatchListActivity : AppCompatActivity() {
